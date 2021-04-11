c83e0cefb719972d4be5b05a7ac5e430530e03ce
sangeetpandey847 <pandeysanj716@gmail.com>
2021-04-12 00:09:48 +0530

    hello program

---
+++ src/helloworldprogram/Main.java
@@ -0,0 +1,15 @@
+
+
+package helloworldprogram;
+
+
+public class Main {
+
+   
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Welcome");
+
+    }
+
+}
